6059dd8afa8585a49b70ed5ac580050ae2ad1ff8
horkar667 <ec2-user@ip-172-31-67-146.ec2.internal>
2021-08-10 02:40:26 +0000

    [Finish]アプリケーション開発練習

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'bebd366b4837cfdc2f383224f6c4797e91dd15886d1572be0073cc99e78204306d25c39a375d3411f72e04686837c7f91b08274e2d824fb814e3f74b1fc4a022'
